6ca6aedca3a3da90c6bf8520e3d980dccca64fef
israel ruiz <israelruiz2005@gmail.com>
2023-08-19 22:25:11 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./App.css";
+import PaginaInicial from "./componentes/PaginaInicial";
+
+export default function App() {
+  return (
+    <div className="App">
+      <PaginaInicial />
+    </div>
+  );
+}
